5e6c6fa49a34805f0ea4580e37ae468aa3ca5b72
Piotr Pepliński <p.peplaj@gmail.com>
2024-11-17 14:28:56 +0100

    finished project

--- carsBlog/blog/views.py
+++ carsBlog/blog/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .models import BlogPost
 
 # Create your views here.
+
+
 def home(request):
     posts = BlogPost.objects.all()
     return render(request, 'home.html', {'posts': posts})
+
+
+def detail(request, post_id):
+    post = BlogPost.objects.get(id=post_id)
+    return render(request, 'detail.html', {'post': post})
